776af187ab1c25bbd13a58014228214d60cb9f94
Antony <antony.budianto@tokopedia.com>
2020-01-02 21:58:13 +0700

    update content

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,28 +1,43 @@
 require(`dotenv`).config({
-  path: `.env`,
-})
+  path: `.env`
+});
 
 module.exports = {
   siteMetadata: {
-    siteTitleAlt: `Cara - Gatsby Starter Portfolio`,
+    // Used for the title template on pages other than the index site
+    siteTitle: `Antony Budianto`,
+    // Default title of the page
+    siteTitleAlt: `Antony Budianto`,
+    // Can be used for e.g. JSONLD
+    siteHeadline: `Antony Budianto`,
+    // Will be used to generate absolute URLs for og:image etc.
+    siteUrl: `https://antonybudianto.com`,
+    // Used for SEO
+    siteDescription: `Antony Budianto is software engineer and open-source contributor`,
+    // Will be set on the <html /> tag
+    siteLanguage: `en`,
+    // Used for og:image and must be placed inside the `static` folder
+    siteImage: `/banner.jpg`,
+    // Twitter Handle
+    author: `@antonybudianto`
   },
   plugins: [
     {
       resolve: `@lekoarts/gatsby-theme-cara`,
-      options: {},
+      options: {}
     },
     {
       resolve: `gatsby-plugin-google-analytics`,
       options: {
-        trackingId: process.env.GOOGLE_ANALYTICS_ID,
-      },
+        trackingId: process.env.GOOGLE_ANALYTICS_ID
+      }
     },
     {
       resolve: `gatsby-plugin-manifest`,
       options: {
-        name: `Cara - @lekoarts/gatsby-theme-cara`,
-        short_name: `Cara`,
-        description: `Playful and Colorful One-Page portfolio featuring Parallax effects and animations`,
+        name: `Antony Budianto`,
+        short_name: `Antony Budianto`,
+        description: `Official Website of Antony Budianto`,
         start_url: `/`,
         background_color: `#141821`,
         theme_color: `#f6ad55`,
@@ -31,17 +46,17 @@ module.exports = {
           {
             src: `/android-chrome-192x192.png`,
             sizes: `192x192`,
-            type: `image/png`,
+            type: `image/png`
           },
           {
             src: `/android-chrome-512x512.png`,
             sizes: `512x512`,
-            type: `image/png`,
-          },
-        ],
-      },
+            type: `image/png`
+          }
+        ]
+      }
     },
     `gatsby-plugin-offline`,
-    `gatsby-plugin-netlify`,
-  ],
-}
+    `gatsby-plugin-netlify`
+  ]
+};
